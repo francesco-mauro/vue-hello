0efc9c831d2ad502cb9cc0d24a66192cdbe03a2d
francesco-mauro <francesco.mauro180@gmail.com>
2024-04-17 18:42:06 +0200

    add: img of a potato

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ createApp({
     data() {
         return {
             message: "questo messaggio sono finalmente riuscito a stamparlo in pagina",
+            myImg: "./img/Patata.png"
         }
     },
 }).mount(`#app`)
